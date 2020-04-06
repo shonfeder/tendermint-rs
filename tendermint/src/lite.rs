3406f592cee7821b35f3615b469940451af2e7a5
Romain Ruetschi <romain@informal.systems>
2020-04-06 13:58:22 +0200

    Make the predicate sketch compile

--- tendermint/src/lite.rs
+++ tendermint/src/lite.rs
@@ -1,6 +1,7 @@
 //! Core logic and traits of a light client.
 
 pub mod error;
+pub mod predicates;
 pub mod types;
 pub mod verifier;
 
--- tendermint/src/lite/predicates.rs
+++ tendermint/src/lite/predicates.rs
@@ -1,162 +1,248 @@
-/// Sketch out the pseudo code for a light client
-/// That integrates the learning from the last iteration.
-/// What we want:
-/// + Simple light client specific types, no crypto
-/// + Crypto can abstracted into traits which implement crypto specific functions
-/// + Express the core verification logic as a composition of predicates to allow mocking
-use crate::Hash;
+//! Sketch out the pseudo code for a light client
+//! That integrates the learning from the last iteration.
+//! What we want:
+//! + Simple light client specific types, no crypto
+//! + Crypto can abstracted into traits which implement crypto specific functions
+//! + Express the core verification logic as a composition of predicates to allow mocking
 
+#![allow(dead_code, unreachable_code)]
 
-// Some Simplified type which only have the fields needed for core verification
+use derive_more::Display;
+use std::time::{Duration, SystemTime};
+
+// Some simplified types which only have the fields needed for core verification
+
+type Hash = u64;
 type Height = u64;
+
+#[derive(Debug, Copy, Clone)]
+enum Error {
+    InvalidCommit,
+    InvalidValidatorSet,
+    InvalidNextValidatorSet,
+    InvalidCommitValue,
+    ImplementationSpecific,
+    NonIncreasingHeight,
+    NonMonotonicBftTime,
+    InsufficientVotingPower,
+}
+
+#[derive(Clone, Debug, Display)]
+#[display(fmt = "{:?}", self)]
 struct Header {
     height: Height,
-    bft_time: systemTime,
+    bft_time: SystemTime,
     validator_set_hash: Hash,
     next_validator_set_hash: Hash,
     hash: Hash, // What if we don't have this
 }
 
+#[derive(Clone, Debug, Display)]
+#[display(fmt = "{:?}", self)]
 struct ValidatorSet {
     hash: Hash,
 }
 
+#[derive(Clone, Debug, Display)]
+#[display(fmt = "{:?}", self)]
 struct Commit {
     header_hash: Hash,
 }
 
+#[derive(Clone, Debug, Display)]
+#[display(fmt = "{:?}", self)]
 struct TrustLevel {
     numerator: u64,
     denominator: u64,
 }
 
+#[derive(Clone, Debug, Display)]
+#[display(fmt = "{:?}", self)]
+struct SignedHeader {
+    header: Header,
+    commit: Commit,
+    validators: ValidatorSet,
+    validator_hash: Hash,
+}
+
+#[derive(Clone, Debug, Display)]
+#[display(fmt = "{:?}", self)]
+struct TrustedState {
+    header: Header,
+    validators: ValidatorSet,
+}
+
 // Crypto function traits allowing mocking out during testing
 trait VotingPowerCalculator: Sized {
     // What kind of errors should we be reporting here?
-    fn voting_power_in(commit: &Commit, validators: &Set) -> Result<u64, Error>;
+    fn voting_power_in(&self, commit: &Commit, validators: &ValidatorSet) -> Result<u64, Error>;
+    fn total_power_of(&self, validators: &ValidatorSet) -> Result<u64, Error>;
 }
 
 trait CommitValidator: Sized {
-    fn validate(commit: &Commit, validators: ValidatorSet) -> Result<(), Error>;
+    fn validate(&self, commit: &Commit, validators: &ValidatorSet) -> Result<(), Error>;
 }
 
 trait HeaderHasher: Sized {
-    fn hash(header: Header) -> Hash; // Or Error?
+    fn hash(&self, header: &Header) -> Hash; // Or Error?
 }
 
 /// Predicates
 
 fn validator_sets_match(signed_header: &SignedHeader, validators: &ValidatorSet) -> bool {
-    return signed_header.validator_hash == validators.hash
+    signed_header.validator_hash == validators.hash
 }
 
 fn next_validators_match(signed_header: &SignedHeader, validators: &ValidatorSet) -> bool {
-    return signed_header.validator_hash == validators.hash
+    signed_header.validator_hash == validators.hash
 }
 
 fn header_matches_commit(
     header: &Header,
     commit: &Commit,
-    header_hasher: HeaderHasher) -> bool {
-    return header_hasher.hash(header.hash) == commit.header_hash
+    header_hasher: &impl HeaderHasher,
+) -> bool {
+    header_hasher.hash(header) == commit.header_hash
 }
 
-fn valid_commit(commit: &Commit, validator: &CommitValidator) -> bool {
-    return validator.validate(commit).is_ok();
+fn valid_commit(
+    commit: &Commit,
+    validators: &ValidatorSet,
+    validator: &impl CommitValidator,
+) -> bool {
+    validator.validate(commit, validators).is_ok()
 }
 
-fn is_within_trusted_period(header: &Header, trusting_period: &Duration, now: &SystemTime) -> bool {
-    let header_time: SystemTime = last_header.bft_time.into();
-    let expires_at = header_time.add(trusting_period);
+fn is_within_trusted_period(header: &Header, trusting_period: Duration, now: SystemTime) -> bool {
+    let header_time: SystemTime = header.bft_time.into();
+    let expires_at = header_time + trusting_period;
 
-    return header_time < now && expired_at > now
+    header_time < now && expires_at > now
 }
 
 fn is_monotonic_bft_time(header_a: &Header, header_b: &Header) -> bool {
-    return header_b.bft_time >= header_a.bft_time
+    header_b.bft_time >= header_a.bft_time
 }
 
 fn is_monotonic_height(header_a: &Header, header_b: &Header) -> bool {
-    return header_b.height > header_b.height
+    header_a.height > header_b.height
 }
 
 fn has_sufficient_voting_power(
     commit: &Commit,
-    validators: &Validators,
-    calculator: &VotingPowerCalculator,
-    trust_level: &TrustingLevel) -> bool {
+    validators: &ValidatorSet,
+    trust_level: &TrustLevel,
+    calculator: &impl VotingPowerCalculator,
+) -> bool {
+    let total_power = calculator.total_power_of(validators);
+    let voting_power = calculator.voting_power_in(commit, validators);
 
-    let voting_power = calculator.voting_power_in(commit, validator_set);
-
-    // XXX: Maybe trust_level doesn't need a very sophisticated type
-    return voting_power > trusted_level;
+    if let (Ok(total_power), Ok(voting_power)) = (total_power, voting_power) {
+        // XXX: Maybe trust_level doesn't need a very sophisticated type
+        voting_power * trust_level.denominator > total_power * trust_level.numerator
+    } else {
+        false
+    }
 }
 
 fn has_sufficient_validators_overlap(
     untrusted_commit: &Commit,
     trusted_validators: &ValidatorSet,
     trust_level: &TrustLevel,
-    calculator: &VotingPowerCalculator) -> bool {
-
-    return has_sufficient_voting_power(untrusted_commit, trusted_validators, trust_level, calculator);
+    calculator: &impl VotingPowerCalculator,
+) -> bool {
+    has_sufficient_voting_power(
+        untrusted_commit,
+        trusted_validators,
+        trust_level,
+        calculator,
+    )
 }
 
 fn has_sufficient_signers_overlap(
     untrusted_commit: &Commit,
     untrusted_validators: &ValidatorSet,
     trust_level: &TrustLevel,
-    calculator: &VotingPowerCalculator) -> bool {
-
-    return has_sufficient_voting_power(untrusted_commit, untrusted_validators, trust_level, calculator);
+    calculator: &impl VotingPowerCalculator,
+) -> bool {
+    has_sufficient_voting_power(
+        untrusted_commit,
+        untrusted_validators,
+        trust_level,
+        calculator,
+    )
 }
-
-// TODO: Parameterize by predicates instead of raw types
-fn verify(
+fn invalid_next_validator_set(
     trusted_state: &TrustedState,
     untrusted_sh: &SignedHeader,
-    untrusted_vals: &ValidatorSet,
     untrusted_next_vals: &ValidatorSet,
-    trust_level: &TrustLevel) -> Result<(), Error> { // shouldn't this return a new TrustedState?
+) -> bool {
+    untrusted_sh.header.height == trusted_state.header.height
+        && trusted_state.validators.hash != untrusted_next_vals.hash
+}
 
-    if !validator_sets_match(untrusted_sh.validators, untrusted_vals) {
-        return InvalidValidatorSet
+fn verify(
+    trusted_state: TrustedState,
+    untrusted_sh: SignedHeader,
+    untrusted_vals: ValidatorSet,
+    untrusted_next_vals: ValidatorSet,
+    trust_level: TrustLevel,
+
+    // Operations
+    validator: impl CommitValidator + Clone,
+    calculator: impl VotingPowerCalculator + Clone,
+    header_hasher: impl HeaderHasher + Clone,
+) -> Result<(), Error> {
+    // shouldn't this return a new TrustedState?
+
+    if !validator_sets_match(&untrusted_sh, &untrusted_vals) {
+        return Err(Error::InvalidValidatorSet);
     }
 
-    if !next_validators_match(unrtusted_sh, untrusted_next_vals) {
-        return InvalidNextValidatorSet
+    if !next_validators_match(&untrusted_sh, &untrusted_next_vals) {
+        return Err(Error::InvalidNextValidatorSet);
     }
 
-    if !header_matches_commit(untrusted_sh) {
-        return InvalidCommitValue
+    if !header_matches_commit(&untrusted_sh.header, &untrusted_sh.commit, &header_hasher) {
+        return Err(Error::InvalidCommitValue);
     }
 
-    if !valid_commit(untrusted_sh.commit) {
-        return ImplementationSpecific
+    if !valid_commit(&untrusted_sh.commit, &untrusted_sh.validators, &validator) {
+        return Err(Error::ImplementationSpecific);
     }
 
-    if !is_monotonic_bft_time(untusted_sh, trusted_state.header) {
-        return NonIncreasingHeight
+    if !is_monotonic_bft_time(&untrusted_sh.header, &trusted_state.header) {
+        return Err(Error::NonMonotonicBftTime);
     }
 
-    if !is_monotonic_height(trusted_state.header, untrusted_sh.header) {
-        return NonIncreasingHeight
+    if !is_monotonic_height(&trusted_state.header, &untrusted_sh.header) {
+        return Err(Error::NonIncreasingHeight);
     }
 
     // XXX: why not integrate this into next_validators_match check?
-    if untrusted_sh.header.height == trusted_state.header.height &&
-        trusted_state.validators.hash != untrusted_next_vals.hash {
-            return InvalidNextValidatorSet
+    if !invalid_next_validator_set(&trusted_state, &untrusted_sh, &untrusted_next_vals) {
+        return Err(Error::InvalidNextValidatorSet);
     }
 
-    if !has_sufficient_validators_overlap(untrusted_sh.commit, trusted_state.validators, trust_level) {
-        return InsufficientVotingPower
+    if !has_sufficient_validators_overlap(
+        &untrusted_sh.commit,
+        &trusted_state.validators,
+        &trust_level,
+        &calculator,
+    ) {
+        return Err(Error::InsufficientVotingPower);
     }
 
-    if !has_sufficient_signers_overlap(untrusted_sh.commit, untrusted_vals) {
-        return InvalidCommit
+    if !has_sufficient_signers_overlap(
+        &untrusted_sh.commit,
+        &untrusted_vals,
+        &trust_level,
+        &calculator,
+    ) {
+        return Err(Error::InvalidCommit);
     }
 
-    return Ok(())
+    Ok(())
 }
 
 //  TODO: Now do the bisection logic as a sequence of verify applications
